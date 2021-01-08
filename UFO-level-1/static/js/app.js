3b45f9610416f7ebb1b57b395efd5178297f4b7b
Justin <gree2167@umn.edu>
2021-01-07 23:41:05 -0600

    added event handlers

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
+//requirements:
+// event handler calls function to filter data, grab input value, filter method to get new table based off of given input, prevent page from reloading with d3
 
-// YOUR CODE HERE!
+// select button and form
+var button = d3.select("#filter-btn");
+var form = d3.select("#form");
+
+// create event handlers for clicking button or pressing enter key
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//create function to run for both events
+function runEnter() {
+
+    //prevent page from refreshing
+    d3.event.preventDefault();
+
+    
+}
+
